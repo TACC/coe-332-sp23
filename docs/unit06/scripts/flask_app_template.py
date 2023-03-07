2045c81607dbdaf549a0c3a31ee372b640e60078
wjallen <wallen@tacc.utexas.edu>
2023-03-07 09:52:14 -0600

    adding sample data and script to unit 6

---
+++ docs/unit06/scripts/flask_app_template.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello, world!\n'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
